572fcafcc63331eaf3d8cb27e4dd74c8c6b311e9
YASSINE <contact@yassine.fun>
2024-08-07 05:28:45 -0700

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- List Glam rock bands by their longevity
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+SELECT band_name, 
+       IFNULL(GREATEST(IFNULL(split, 2022) - formed, 0), 0) AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;

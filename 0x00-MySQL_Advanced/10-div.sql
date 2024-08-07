6910fe10fd2a14d3226ba5fc745005c6d116a051
YASSINE <contact@yassine.fun>
2024-08-07 05:33:45 -0700

    Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,15 +1,16 @@
 -- Drop existing SafeDiv function if it exists
 DROP FUNCTION IF EXISTS SafeDiv;
 
--- Create function SafeDiv with high precision
 DELIMITER //
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DOUBLE PRECISION DETERMINISTIC
+RETURNS DECIMAL(10,6) DETERMINISTIC
 BEGIN
     IF b = 0 THEN
         RETURN 0;
     ELSE
-        RETURN a * 1.0 / b;
+        RETURN a / b;
     END IF;
 END //
+
 DELIMITER ;
